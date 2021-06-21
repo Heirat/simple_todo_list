b327b8382d3da25d2d714cfce8461c144d817505
Heirat <neocron76@gmail.com>
2021-06-21 20:52:39 +0300

    feat: append new task to active-list ul

--- js/script.js
+++ js/script.js
@@ -22,10 +22,47 @@ window.addEventListener('beforeunload', e => {
     localStorage.setItem('tasks', JSON.stringify(tasks))
 })
 
+function appendToActiveList(taskText) {
+    let newItem = document.createElement('li')
+    newItem.classList.add('list-item')
+    newItem.innerHTML =
+        `
+            <span class = "text">${taskText}</span>
+            <div class = "list-item-buttons">
+                <button class = "btn btn_colored btn-complete">Complete</button>
+                <button class = "btn btn_colored btn-remove">Remove</button> 
+            </div>
+        `
+    activeList.append(newItem)
+}
+
+function addActive(taskText) {
+    if (taskText && !tasks.active.includes(taskText)) {
+        tasks.active.push(taskText)
+        appendToActiveList(taskText)
+        viewActiveList();
+    }
+}
+
+function viewActiveList() {
+    if (tasks.active.length > 0)
+        wrapperActive.classList.remove('d-none')
+    else
+        wrapperActive.classList.add('d-none')
+}
+
+function viewCompleteList() {
+    if (tasks.completed.length > 0)
+        wrappeCompleted.classList.remove('d-none')
+    else
+        wrapperCompleted.classList.add('d-none')
+}
+
+// Add task to tasks.active
+
 btnAdd.addEventListener('click', () => {
-    if (inputNew.value && !tasks.active.includes(inputNew.value))
-        tasks.active.push(inputNew.value);
-    console.log(tasks.active);
+    addActive(inputNew.value)
+    inputNew.value = ''
 })
 
 inputNew.addEventListener('keyup', e => {
